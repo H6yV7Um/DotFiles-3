1a355639fc035fe1e8c1e7714a819caee88fbd7e
jinleileiking <jinleileiking@gmail.com>
2012-03-16 14:48:57 +0800

    merged from mic. and fuck!

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -1,4 +1,8 @@
+print("[awesome] Entered awesome.lua: "..os.date())
+
+
 -- {{{ library
+
 require("vicious")
 require("awful")
 require("awful.autofocus")
@@ -8,6 +12,9 @@ require("beautiful")
 -- Notification library
 require("naughty")
 require("plugins/revelation")
+require("plugins/lognotify")
+require("cal")
+require("markup")
 -- }}}
 
 -- {{{ Theme
@@ -21,6 +28,31 @@ beautiful.init(awful.util.getdir("config") .. "/themes/default/theme.lua")
 
 -- }}}
 
+-- {{{ Error handling
+-- Check if awesome encountered an error during startup and fell back to
+-- another config (This code will only ever execute for the fallback config)
+if awesome.startup_errors then
+    naughty.notify({ preset = naughty.config.presets.critical,
+                     title = "Oops, there were errors during startup!",
+                     text = awesome.startup_errors })
+end
+
+-- Handle runtime errors after startup
+do
+    local in_error = false
+    awesome.add_signal("debug::error", function (err)
+        -- Make sure we don't go into an endless error loop
+        if in_error then return end
+        in_error = true
+
+        naughty.notify({ preset = naughty.config.presets.critical,
+                         title = "Oops, an error happened!",
+                         text = err })
+        in_error = false
+    end)
+end
+-- }}}
+
 -- {{{ AutorunOnce
 local r = require("runonce")
 
@@ -30,20 +62,15 @@ autorun_items =
     "VBoxClient-all",
     "gvim",
     "ibus-daemon -d -x",
-    "chromium"
+    "chromium",
+    "pcmanfm"
 }
 
 for index, item in ipairs(autorun_items) do
   r.run(item)
 end
-
---awful.util.spawn_with_shell("ibus-daemon -d -x")
---awful.util.spawn_with_shell("VBoxClient-all")
---awful.util.spawn_with_shell("gvim")
---awful.util.spawn_with_shell("terminator -e irssi")
---awful.util.spawn_with_shell("terminator")
---awful.util.spawn_with_shell("chromium")
 -- }}}
+
 
 -- {{{ Values
 -- This is used later as the default terminal and editor to run.
@@ -58,9 +85,10 @@ term_cmd = terminal .. " -e "
 -- I suggest you to remap Mod4 to another key using xmodmap or other tools.
 -- However, you can use another modifier like Mod1, but it may interact with others.
 modkey = "Mod4"
+local icon_path = awful.util.getdir("config").."/icons/"
 -- }}}
 
--- {{{Table of layouts to cover with awful.layout.inc, order matters.
+-- {{{Layouts
 layouts =
 {
     awful.layout.suit.tile,
@@ -79,14 +107,8 @@ layouts =
 -- }}}
 
 -- {{{ Tags
- --Define a tag table which hold all screen tags.
---tags = {}
---for s = 1, screen.count() do
-     --Each screen has its own tag table.
-    --tags[s] = awful.tag({ 1, "gvim", "Chrome", "irssi", 5, 6, 7, 8, 9 }, s, layouts[1])
---end
 tags = {
-    names  = { "term", "gvim", "Chrome", 4, 5, 6, 7, 8, 9 },
+    names  = { "term", "gvim", "Chrome", 4, 5, 6, 7, 8, "pcmanfm" },
     layout = { layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1]}
 }
 for s = 1, screen.count() do
@@ -98,21 +120,39 @@ end
 -- {{{ Menu
 -- Create a laucher widget and a main menu
 myawesomemenu = {
-   --{ "manual", terminal .. " -e man awesome" },
-   --{ "edit config", editor_cmd .. " " .. awful.util.getdir("config") .. "/rc.lua" },
-   --{ "gvim", shell .. " gvim"},
    { "manual", term_cmd .. "\'man awesome\'" },
-   --{ "edit config", term_cmd .. "\'gvim " .. awful.util.getdir("config") .. "/rc.lua" .. "\'" },
    { "edit config", shell_cmd .. "\'gvim " .. awful.util.getdir("config") .. "/rc.lua" .. "\'" },
+   { "powersafe off", "xset s off" },
+   { "xrandr", "xrandr --auto" },
+   { "arandr", "arandr" },
    { "restart", awesome.restart },
    { "quit", awesome.quit }
 }
---mystatusbar = awful.wibox({ position = "bottom", screen = 1, ontop = false, width = 1, height = 16 })
 
+-- reboot/shutdown as user using Consolkit and shutdown/hibernate using upower
+-- Make sure you using ck-launch-session to start awesome and you are in the power group.
+-- emerge consolekit upower
+-- rc-update add consolekit default
+
+local upower = [[dbus-send --print-reply \
+--system \
+--dest=org.freedesktop.UPower \
+/org/freedesktop/UPower \
+org.freedesktop.UPower.]]
+local consolkit = [[dbus-send --print-reply \
+--system \
+--dest="org.freedesktop.ConsoleKit" \
+/org/freedesktop/ConsoleKit/Manager \
+org.freedesktop.ConsoleKit.Manager.]]
 
 mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
                                     { "terminal", terminal },
-                                    { "chromium", shell_cmd .. "chromium"}
+                                    { "chromium", shell_cmd .. "chromium"},
+                                    { "lock", "slock" },
+                                    { "Suspend", function() awful.util.spawn(upower.."Suspend") end },
+                                    { "Hibernate", function () awful.util.spawn(upower.."Hibernate") end },
+                                    { "Restart", consolkit.."Restart", icon_path.."restart.png" },
+                                    { "Shutdown", consolkit.."Stop", icon_path.."poweroff.png" },
                                   }
                         })
 
@@ -120,69 +160,120 @@ mylauncher = awful.widget.launcher({ image = image(beautiful.awesome_icon),
                                      menu = mymainmenu })
 -- }}}
 
--- {{{ Systray
--- Create a textclock widget
-mytextclock = awful.widget.textclock({ align = "right" })
+-- {{{ Naughty log notify
+print("[awesome] Enable naughty log notify")
+ilog = lognotify{
+   logs = {
+      emerge_fetch = { file = "/var/log/emerge_fetch.log", },
+      emerge = { file = "/var/log/emerge.log", },
+      elog = { file = "/var/log/portage/elog/summary.log", },
+   },
+   interval = 1,
+   naughty_timeout = 15
+}
+ilog:start()
+-- }}}
 
---Initialize widget
+
+--{{{Date
+
 datewidget = widget({ type = "textbox" })
--- Register widget
 vicious.register(datewidget, vicious.widgets.date, "%y-%m-%d %H:%M:%S", 1)
+local clockicon = widget({ type = "imagebox" })
+clockicon.image = image(icon_path.."clock.png")
+
+cal.register(clockicon, markup.fg("#FF5656","<b>%s</b>"), true)
+local uptimetooltip = awful.tooltip({})
+uptimetooltip:add_to_object(datewidget)
+datewidget:add_signal("mouse::enter",  function()
+  local args = vicious.widgets.uptime()
+  local text = (" <b>Uptime</b> %dd %dh %dmin "):format(args[1], args[2], args[3])
+  uptimetooltip:set_text(text)
+end)
+---}}}
 
 
---  Initialize widget
---memwidget = awful.widget.progressbar()
----- Progressbar properties
---memwidget:set_width(8)
---memwidget:set_height(10)
---memwidget:set_vertical(true)
---memwidget:set_background_color("#494B4F")
---memwidget:set_border_color(nil)
---memwidget:set_color("#AECF96")
---memwidget:set_gradient_colors({ "#AECF96", "#88A175", "#FF5656" })
----- Register widget
---vicious.register(memwidget, vicious.widgets.mem, "$1", 13)
 
- --Initialize widget
+-- {{{ Battery
+local batwidget = widget({ type = "textbox" })
+local baticon   = widget({ type = "imagebox"})
+baticon.image   = image(icon_path.."bat.png")
+local batbar    = awful.widget.progressbar()
+
+-- Progressbar properties
+batbar:set_width(8)
+batbar:set_height(20)
+batbar:set_vertical(true)
+batbar:set_background_color(beautiful.fg_off_widget)
+batbar:set_color(beautiful.fg_widget)
+batbar:set_gradient_colors({ '#FF5656', '#88A175', '#AECF96'})
+awful.widget.layout.margins[batbar.widget] = { top = 2, bottom = 2, left = 1, right = 2 }
+
+vicious.cache(vicious.widgets.bat)
+vicious.register(batbar, vicious.widgets.bat, "$2",  11, "BAT1")
+vicious.register(batwidget, vicious.widgets.bat, "$1$2% $3h", 11, "BAT1")
+-- }}}
+
+
+---{{{ Mem
 memwidget_tb = widget({ type = "textbox" })
--- Register widget
 vicious.register(memwidget_tb, vicious.widgets.mem, "$1%($2M/$3M)", 13)
+mem_icon = widget({ type = "imagebox"})
+mem_icon.image = image(icon_path.."mem.png")
+---}}}
 
 
- --Initialize widget
-cpuwidget = widget({ type = "textbox" })
--- Register widget
-vicious.register(cpuwidget, vicious.widgets.cpu, "$1%")
+-- {{{ CPU usage
+local cpuwidget = widget({ type = "textbox" })
+local cpuicon = widget({ type = "imagebox" }); cpuicon.image = image(icon_path.."cpu.png")
+-- Initialize widgets
+vicious.register(cpuwidget, vicious.widgets.cpu,
+function (widget, args)
+  local text
+  -- list all cpu cores
+  for i=1,#args do
+    -- alerts, if system is stressed
+    --args[i] = markup.fg(markup.gradient(1,100,args[i]),args[i])
+    if args[i] > 90 then
+      args[i] = markup.fg("#FF5656", args[i]) -- light red
+    elseif args[i] > 70 then
+      args[i] = markup.fg("#AECF96", args[i]) -- light green
+    end
+
+    -- append to list
+    if i > 2 then text = text.."/"..args[i].."%"
+    else text = args[i].."%" end
+  end
+  return text
+end)
+-- Register buttons
+cpuwidget:buttons( awful.button({ }, 1, function () awful.util.spawn(terminal .. " -e top") end) )
+cpuicon:buttons( cpuwidget:buttons() )
+
+-- }}}
 
 
---Initialize widget
---cpuwidget_graph = awful.widget.graph()
----- Graph properties
---cpuwidget_graph:set_width(50)
---cpuwidget_graph:set_background_color("#494B4F")
---cpuwidget_graph:set_color("#FF5656")
---cpuwidget_graph:set_gradient_colors({ "#FF5656", "#88A175", "#AECF96" })
----- Register widget
---vicious.register(cpuwidget_graph, vicious.widgets.cpu, "$1")
+-- {{{ CPU temperature
+local thermalwidget = widget({ type = "textbox" })
+local thermalicon = widget({ type = "imagebox" })
+thermalicon.image = image(icon_path.."temp.png")
+vicious.register(thermalwidget, vicious.widgets.thermal, "$1°C", 5, {"thermal_zone0", "sys"})
+-- }}}
 
-  --Network usage widget
- --Initialize widget
+--{{{Network usage widget
 netwidget = widget({ type = "textbox" })
--- Register widget
---vicious.register(netwidget, vicious.widgets.net, '|<span color="#CC9393">D ${eth0 down_kb}</span> <span color="#7F9F7F">U ${eth0 up_kb}</span>', 3)
+local neticon  = widget({ type = "imagebox" }); neticon.image = image(icon_path.."ethernet.png")
 vicious.register(netwidget, vicious.widgets.net, '<span color="#CC9393">⇩${eth0 down_kb}</span> <span color="#7F9F7F">⇧${eth0 up_kb}</span>', 3)
---dnicon = widget({ type = "imagebox" })
---upicon = widget({ type = "imagebox" })
---dnicon.image = image(beautiful.widget_net)
---upicon.image = image(beautiful.widget_netup)
+---}}}
 
-cpu_icon = widget({ type = "imagebox" })
-cpu_icon.image = image(beautiful.cpu)
-mem_icon = widget({ type = "imagebox" })
-mem_icon.image = image(beautiful.mem)
-clock_icon = widget({ type = "imagebox" })
-clock_icon.image = image(beautiful.clock)
+-- {{{ Disk I/O
+local ioicon = widget({ type = "imagebox" })
+ioicon.image = image(icon_path.."disk.png") ioicon.visible = true
+local iowidget = widget({ type = "textbox" })
+vicious.register(iowidget, vicious.widgets.dio, "⇧${sda read_mb}⇩${sda write_mb}M", 3)
+-- }}}
 
+--{{{ systray
 -- Create a systray
 mysystray = widget({ type = "systray" })
 
@@ -255,14 +346,13 @@ for s = 1, screen.count() do
             layout = awful.widget.layout.horizontal.leftright
         },
         mylayoutbox[s],
-        --mytextclock,
-        datewidget, clock_icon,
-        --upicon, netwidget, dnicon,
-        --memwidget,
+        datewidget, clockicon,
         memwidget_tb, mem_icon,
-        cpuwidget, cpu_icon,
-        --cpuwidget_graph,
-        netwidget,
+        cpuwidget, cpuicon,
+        iowidget, ioicon,
+        netwidget, neticon,
+        batbar.widget, batwidget, baticon,
+        thermalwidget, thermalicon,
         s == 1 and mysystray or nil,
         mytasklist[s],
         layout = awful.widget.layout.horizontal.rightleft
@@ -317,6 +407,7 @@ globalkeys = awful.util.table.join(
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
     awful.key({                   }, "Print", function  () awful.util.spawn("scrot -e 'mv $f ~/ 2>/dev/null'") end),
+    awful.key({ modkey,           }, "z", function  () awful.util.spawn("slock") end),
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
 
@@ -462,6 +553,16 @@ client.add_signal("focus", function(c) c.border_color = beautiful.border_focus e
 client.add_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
 
+
+-- {{{ Welcome Message
+print("[awesome] Send welcome message")
+
+naughty.notify{
+  title = "Awesome "..awesome.version.." started!",
+  text  = string.format("Welcome %s. Your host is %s.\nIt is %s",
+  os.getenv("USER"), awful.util.pread("hostname"):match("[^\n]*"), os.date()),
+  timeout = 7 }
+-- }}}
 
 -- vim:fen:fdm=marker
 
